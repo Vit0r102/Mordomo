3ca37d44cf6bbdaf9c2dcad8376fc225696605b4
Vitor <m.vitordesouza033@gmail.com>
2026-09-08 17:13:00 -0300

    Organizando um texto no sidebar

--- src/components/mordomo/AppShell.jsx
+++ src/components/mordomo/AppShell.jsx
@@ -66,7 +66,7 @@ export function AppShell({ children }) {
             <Quote size={20} />
           </span>
           <span>
-            <span className="md-brand-name">Mordomo</span>
+            <span className="md-brand-name">Mordomo</span> <br />
             <span className="md-brand-tag">Gestão com propósito</span>
           </span>
         </div>
